2c871d3d121f0a14c8f5ea7716b80c3c0369abcc
Benson Nastor <mnastor@deloitte.com>
2023-07-14 22:15:46 +0800

    Initial commit

---
+++ routes/mockdata.js
@@ -0,0 +1,54 @@
+const userRoutes = (app, fs) => {
+
+    const ENV_DIR = 'sit'
+
+    const getDataPath = (req) => {
+      const path1 = req.params['0'].replace('/api/', '');
+      const subPaths = path1.split('/').slice(-2);
+      console.log('dbg', {subPaths})
+      var joined = subPaths.join('_').trim('_')
+      if( joined.charAt( 0 ) === '_' ){
+        joined = joined.slice( 1 );
+      }
+      return joined;
+    }
+
+    app.get('*', (req, res) => {
+      const dataPath = `./data/${ENV_DIR}/${getDataPath(req)}.json`
+      fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+          res.send({ message: `Cannot find ${dataPath}` });
+          return;
+        }
+  
+        res.send(JSON.parse(data));
+      });
+    });
+
+    app.post('*', (req, res) => {
+      const dataPath = `./data/${ENV_DIR}/post_${getDataPath(req)}.json`
+      fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+          res.send({ message: `Cannot find ${dataPath}` });
+          return;
+        }
+  
+        res.send(JSON.parse(data));
+      });
+    });
+
+    app.put('*', (req, res) => {
+      const dataPath = `./data/${ENV_DIR}/put_${getDataPath(req)}.json`
+      fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+          res.send({ message: `Cannot find ${dataPath}` });
+          return;
+        }
+  
+        res.send(JSON.parse(data));
+      });
+    });
+    
+  };
+  
+  module.exports = userRoutes;
---
+++ server.js
@@ -0,0 +1,36 @@
+// load up the express framework and body-parser helper
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create an instance of express to serve our end points
+const app = express();
+app.use(express.json())
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://0.0.0.0:8080');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+
+  next();
+
+});
+
+// we'll load up node's built in file system helper library here
+// (we'll be using this later to serve our JSON files
+const fs = require('fs');
+
+// configure our express instance with some body-parser settings
+// including handling JSON data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// this is where we'll handle our various routes from
+const routes = require('./routes/routes.js')(app, fs);
+
+// finally, launch our server on port 3001.
+const server = app.listen(3001, () => {
+  console.log('listening on port %s...', server.address().port);
+});
